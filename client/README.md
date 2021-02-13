244535e80cc1a0a755bf17204c6dcb6bb92da4ef
shijing0628 <brendaliu0628@gmail.com>
2021-02-12 21:11:50 -0600

    layout of register page

--- client/README.md
+++ client/README.md
@@ -1,8 +1,4 @@
-This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
-
-## Getting Started
-
-First, run the development server:
+### Frontend of reddit app clone
 
 ```bash
 npm run dev
@@ -12,23 +8,16 @@ yarn dev
 
 Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
 
-You can start editing the page by modifying `pages/index.js`. The page auto-updates as you edit the file.
+css shadowlord link: https://noeldelgado.github.io/shadowlord/#a41771 (generate multi color pattern)
+<br>
 
-[API routes](https://nextjs.org/docs/api-routes/introduction) can be accessed on [http://localhost:3000/api/hello](http://localhost:3000/api/hello). This endpoint can be edited in `pages/api/hello.js`.
+![](2021-02-12-20-51-25.png)
 
-The `pages/api` directory is mapped to `/api/*`. Files in this directory are treated as [API routes](https://nextjs.org/docs/api-routes/introduction) instead of React pages.
+<br>
 
-## Learn More
+reference link: https://github.com/hidjou/classsed-reddit-clone
 
-To learn more about Next.js, take a look at the following resources:
+**some commands:**
 
-- [Next.js Documentation](https://nextjs.org/docs) - learn about Next.js features and API.
-- [Learn Next.js](https://nextjs.org/learn) - an interactive Next.js tutorial.
-
-You can check out [the Next.js GitHub repository](https://github.com/vercel/next.js/) - your feedback and contributions are welcome!
-
-## Deploy on Vercel
-
-The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
-
-Check out our [Next.js deployment documentation](https://nextjs.org/docs/deployment) for more details.
+- npm install --save-dev typescript @types/react
+- npm run dev will generate content of tsconfig.json
---
+++ client/src/pages/register.tsx
@@ -0,0 +1,43 @@
+import Head from "next/head";
+import Link from 'next/link'
+
+export default function Register() {
+  return (
+    <div className="flex">
+      <Head>
+        <title>Register</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+     <div className="h-screen bg-center bg-cover w-36" style={{backgroundImage:"url('/images/register.jpg')"}}>
+     </div>
+     <div className="flex flex-col justify-center pl-6">
+       <div className="w-70">
+       <h1 className="mb-2 text-lg font-medium">Sign Up</h1>
+       <p className="mb-10 text-xs">By continuing, you agree to our User Agreement and Privacy Policy.</p>
+       <form>
+         <div className="mb-6">
+           <input type="checkbox" className="mr-1 cursor-pointer" id="agreement"/>
+           <label htmlFor="agreement" className="text-xs cursor-pointer">I agree to get emails about cool stuff on Readit</label>
+         </div>
+         <div className="mb-2">
+          <input className="w-full p-3 px-3 py-2 transition duration-200 border border-gray-300 rounded outline-none bg-gray-50 focus:bg-white hover:bg-white" placeholder="Email" type="email"/>
+         </div>
+          <div className="mb-2">
+          <input className="w-full p-3 px-3 py-2 transition duration-200 border border-gray-300 rounded outline-none bg-gray-50100 focus:bg-white hover:bg-white" placeholder="Username" type="text"/>
+         </div>
+           <div className="mb-2">
+          <input className="w-full p-3 px-3 py-2 transition duration-200 border border-gray-300 rounded outline-none bg-gray-50 focus:bg-white hover:bg-white" placeholder="Password" type="password"/>
+         </div>
+         <button className="w-full py-2 mb-4 text-xs font-bold text-white uppercase transition duration-200 bg-blue-500 border border-blue-500 rounded">Sign Up</button>
+       </form>
+       <small>Already a readitor? 
+         <Link href="/login">
+           <a className="ml-1 text-blue-500 uppercase">Login</a>
+         </Link>
+       </small>
+       </div>
+     </div>
+    </div>
+  );
+}
